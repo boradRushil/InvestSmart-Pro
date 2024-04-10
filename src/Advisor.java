9c0c8e08684be8078071faac746a414301740814
rushil <rushil.borad@dal.ca>
2024-04-10 17:59:36 -0300

    Implemented the methods for adding clients and advisors

--- src/Advisor.java
+++ src/Advisor.java
@@ -1,11 +1,39 @@
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class Advisor {
     private int advisorId;
-    private String advisorName;
+    private String name;
 
-    public Advisor(int advisorId, String advisorName) {
-        this.advisorId = advisorId;
-        this.advisorName = advisorName;
+    // Constructor
+    public Advisor(String name) {
+        this.name = name;
     }
 
-    // Getters and Setters
+    // Save the advisor to the database
+    public int save() {
+        final String insertQuery = "INSERT INTO FinancialAdvisor (Name) VALUES (?)";
+        try (Connection conn = DatabaseConnector.getConnection();
+             PreparedStatement stmt = conn.prepareStatement(insertQuery, Statement.RETURN_GENERATED_KEYS)) {
+            stmt.setString(1, this.name);
+            int affectedRows = stmt.executeUpdate();
+
+            if (affectedRows > 0) {
+                try (ResultSet generatedKeys = stmt.getGeneratedKeys()) {
+                    if (generatedKeys.next()) {
+                        this.advisorId = generatedKeys.getInt(1);
+                        return this.advisorId;
+                    }
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return -1; // Return -1 to indicate failure
+    }
+
+    // Getters and Setters...
 }
--- src/Client.java
+++ src/Client.java
@@ -1,11 +1,39 @@
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class Client {
     private int clientId;
-    private String clientName;
+    private String name;
 
-    public Client(int clientId, String clientName) {
-        this.clientId = clientId;
-        this.clientName = clientName;
+    // Constructor
+    public Client(String name) {
+        this.name = name;
     }
 
-    // Getters and Setters
+    // Save the client to the database
+    public int save() {
+        final String insertQuery = "INSERT INTO Client (Name) VALUES (?)";
+        try (Connection conn = DatabaseConnector.getConnection();
+             PreparedStatement stmt = conn.prepareStatement(insertQuery, Statement.RETURN_GENERATED_KEYS)) {
+            stmt.setString(1, this.name);
+            int affectedRows = stmt.executeUpdate();
+
+            if (affectedRows > 0) {
+                try (ResultSet generatedKeys = stmt.getGeneratedKeys()) {
+                    if (generatedKeys.next()) {
+                        this.clientId = generatedKeys.getInt(1);
+                        return this.clientId;
+                    }
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return -1; // Return -1 to indicate failure
+    }
+
+    // Getters and Setters...
 }
